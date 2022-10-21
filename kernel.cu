fd56cf95a053ed1f90d60e5e4aca595b532dc438
Samuel Líška <samuel.liska49@gmail.com>
2022-10-21 09:59:33 +0200

    Working example for 50 stars but when print - off

--- kernel.cu
+++ kernel.cu
@@ -7,34 +7,30 @@ Performs iteration paraller calculation no GPU
 #define NUM_BLOCKS = 10
 #define NUM_THREADS = 256
 
-__global__ void increment_gpu(sGalaxy A, sGalaxy B, int n, float* result){
+__global__ void increment_gpu(sGalaxy A, sGalaxy B, int n ,float* result){
     //determine unique index within grid, miximizing block usage
-    __shared__ float d_result;
-    //use odd/even numbers to i/j index?
-    int i = threadIdx.x + blockIdx.x * blockDim.x;
+    int i = threadIdx.y + blockIdx.y * blockDim.y;
     int j = blockIdx.x * blockDim.x + threadIdx.x;
-
-
-    //int j = i + 1;
-    if (i < n - 1){
-        d_result = 0.0f;
-        // porovnat A.x[i+1] tu a ked sa buildi ci su rovnake!  
+    if (i < n && j < n && j > i && i != j){
+        float tmp = 0.0f;
         //printf("a.x[i->%d]: %f  a.x[j->%d]: %f\n", i,A.x[i], j, A.x[j]);
-        
         float da = sqrt((A.x[i]-A.x[j])*(A.x[i]-A.x[j])
                     + (A.y[i]-A.y[j])*(A.y[i]-A.y[j])
                     + (A.z[i]-A.z[j])*(A.z[i]-A.z[j]));
         float db = sqrt((B.x[i]-B.x[j])*(B.x[i]-B.x[j])
                     + (B.y[i]-B.y[j])*(B.y[i]-B.y[j])
                     + (B.z[i]-B.z[j])*(B.z[i]-B.z[j]));
-        float tmp = (da-db) * (da-db);
-        printf("[%d][%d](%f-%f) * (%f-%f)\n", i,j,da,db,da,db);
+        tmp = (da-db) * (da-db);
+        atomicAdd(result, tmp);
 
-        d_result += tmp;
-        *result = sqrt(1/((float)n*((float)n-1)) * d_result);
+        printf("[%d][%d](%f-%f) * (%f-%f) -- tmp = %f -- result %f\n", i,j,da,db,da,db,tmp, *result);
+        if(i == n-2 && j == n-1){
+            float diff = *result;
+            *result = sqrt(1/((float)n*((float)n-1)) * diff);
+            printf("RESULT ::  : %f", *result);
+        }
+        return ;
     }
-    return;
-
 }
 
 
@@ -46,11 +42,11 @@ float solveGPU(sGalaxy A, sGalaxy B, int n) {
     cudaMalloc((void**)&d_result, sizeof(float));
 
     //define grid and block sizes
-    dim3 block_size(n,n);
-    dim3 grid_size(1,1);
+    dim3 block_size(16,16);
+    dim3 grid_size((n+15)/16,  (n+15)/16);
 
     //Launch kernel with pass by referennce attribute and N size
-    increment_gpu <<<grid_size, block_size>>>(A, B, n, d_result);
+    increment_gpu <<<grid_size, block_size>>>(A, B, n,d_result);
 
     //copy data from GPU memory to CPU memory via PCIe bus
     cudaMemcpy(h_result, d_result, sizeof(float), cudaMemcpyDeviceToHost);
--- kernel_CPU.C
+++ kernel_CPU.C
@@ -11,10 +11,10 @@ float solveCPU(sGalaxy A, sGalaxy B, int n) {
 				+ (B.z[i]-B.z[j])*(B.z[i]-B.z[j]));
 			//XXX for large galaxies, more precise version of sum should be implemented, not required in this example 
 			tmp += (da-db) * (da-db);
-			printf("[%d][%d](%f-%f) * (%f-%f)\n", i,j,da,db,da,db);
+			//printf("[%d][%d](%f-%f) * (%f-%f) -- tmp = %f\n", i,j,da,db,da,db,tmp);
 		}
 		diff += tmp;
 	}
-			
+	//printf("total diff: %f \n",diff);
 	return sqrt(1/((float)n*((float)n-1)) * diff);
 }
